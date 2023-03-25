f0a77cf320f44d719e53646a9fc749ec6c545a5d
RenanGAS <renansakaoki@gmail.com>
2023-03-25 18:22:55 -0300

    #001: adiciona novos atributos na classe User

--- portal-aulas-api/users/models.py
+++ portal-aulas-api/users/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
   name = models.CharField(max_length=120)
   email = models.CharField(max_length=120)
   password = models.CharField(max_length=120)
-  created_at = models.DateField(auto_now_add=True)
+  created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
 
   def __str__(self):
--- portal-aulas-api/users/serializers.py
+++ portal-aulas-api/users/serializers.py
@@ -1,7 +1,7 @@
 from users.models import User
-from  rest_framework import serializers
+from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
       model = User
-      fields = ['id', 'name', 'created_at']
+      fields = ['id', 'name', 'email', 'password', 'created_at', 'updated_at']
