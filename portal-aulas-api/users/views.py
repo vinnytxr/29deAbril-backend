192b5c994ca558dae80d7eea58d8c83b8d176603
RenanGAS <renansakaoki@gmail.com>
2023-03-27 12:01:07 -0300

    #001: usa ModelViewSet e DefaultRouter para criar views e rotas padrao do django

--- portal-aulas-api/users/views.py
+++ portal-aulas-api/users/views.py
@@ -1,36 +1,8 @@
 from users.models import User
 from users.serializers import UserSerializer
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.exceptions import NotFound
-from rest_framework import status, generics
+from rest_framework import viewsets
 
-class UserListAndCreate(generics.ListCreateAPIView):
+
+class UserViewSet(viewsets.ModelViewSet):
   queryset = User.objects.all()
   serializer_class = UserSerializer
-
-class UserDetailChangeAndDelete(APIView):
-  def get_object(self, pk):
-    try:
-      return User.objects.get(pk=pk)
-    except User.DoesNotExist:
-      raise NotFound()
-    
-  def get(self, request, pk):
-    user = self.get_object(pk)
-    serializer = UserSerializer(user)
-    return Response(serializer.data)
-  
-  def put(self, request, pk):
-    user = self.get_object(pk)
-    serializer = UserSerializer(user, data=request.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-  
-  def delete(self, request, pk):
-    user = self.get_object(pk)
-    user.delete()
-    return Response(status.HTTP_204_NO_CONTENT)
