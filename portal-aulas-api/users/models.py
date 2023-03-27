ce778dea8789f5135ba2ac53c9e17888633e258c
RenanGAS <renansakaoki@gmail.com>
2023-03-27 11:59:18 -0300

    #001: define o email como valor unico

--- portal-aulas-api/users/models.py
+++ portal-aulas-api/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class User(models.Model):
   name = models.CharField(max_length=120)
-  email = models.CharField(max_length=120)
+  email = models.CharField(max_length=120, unique=True)
   password = models.CharField(max_length=120)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
