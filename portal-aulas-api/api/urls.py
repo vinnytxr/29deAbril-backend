84af6074499e21843570bc6ec26439bb998c3984
alessandro <git.aleneves@gmail.com>
2023-03-31 14:39:08 -0300

    #002: adicionando dockers e scripts de deploy

--- portal-aulas-api/api/urls.py
+++ portal-aulas-api/api/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+ 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
+    path('users/', include('users.urls')), 
 ]
