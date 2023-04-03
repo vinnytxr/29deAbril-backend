38f1bf5bd995cb17c1f48e4c3f330bd33644491e
RenanGAS <renansakaoki@gmail.com>
2023-04-02 23:07:25 -0300

    fix: implementa a condicao 'if not exists'

--- init-database-pg.sql
+++ init-database-pg.sql
@@ -1,1 +1,4 @@
-create database if not exists portal_aulas_online;
+CREATE EXTENSION dblink;
+
+SELECT 'CREATE DATABASE portal_aulas_online'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'portal_aulas_online')\gexec
