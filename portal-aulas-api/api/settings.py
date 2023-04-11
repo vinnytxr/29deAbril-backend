de286497d60fa51b6eb0ad50024f92b4055795e7
RenanGAS <renansakaoki@gmail.com>
2023-04-11 15:50:33 -0300

    #4: define configurações para encriptação

--- portal-aulas-api/api/settings.py
+++ portal-aulas-api/api/settings.py
@@ -159,3 +159,8 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = "user.User"
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
+
+AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend',]
+PASSWORD_HASHERS = [
+   'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',  # or any other hashing algorithm you want to use
+]
