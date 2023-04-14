35d06afa6ce5b8a398f3a715bdb5b9f477e18140
RenanGAS <renansakaoki@gmail.com>
2023-04-14 13:00:57 -0300

    #20: arquivo para funções auxiliares - busca de usuário e geração de token

---
+++ portal-aulas-api/user/services.py
@@ -0,0 +1,20 @@
+import datetime
+import jwt
+from . import models
+from django.conf import settings
+import pytz
+
+def fetch_user_by_email(email: str)->"User":
+    user = models.User.objects.filter(email=email).first()
+
+    return user
+
+def create_token(user_id: int)->str:
+    payload = {
+        "id":user_id,
+        "exp":datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(hours=24)
+    }
+
+    token=jwt.encode(payload, settings.JWT_SECRET, algorithm="HS256")
+
+    return token
