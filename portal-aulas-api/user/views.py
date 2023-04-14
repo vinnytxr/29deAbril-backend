85fa3442fbee8c6d6d4c138f3bee2cf6945a4640
RenanGAS <renansakaoki@gmail.com>
2023-04-14 13:02:35 -0300

    #20: cria e implementa rotas para login, logout e recuperação de informações do usuário

--- portal-aulas-api/user/views.py
+++ portal-aulas-api/user/views.py
@@ -1,6 +1,8 @@
 from user.models import User, Role
 from user.serializers import UserSerializer, RoleSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, views, response, exceptions, permissions
+from . import services, authentication
+
 
 class RoleViewSet(viewsets.ModelViewSet):
   queryset = Role.objects.all()
@@ -9,3 +11,46 @@ class RoleViewSet(viewsets.ModelViewSet):
 class UserViewSet(viewsets.ModelViewSet):
   queryset = User.objects.all()
   serializer_class = UserSerializer
+
+class LoginAPIView(views.APIView):
+  def post(self, request):
+    email = request.data["email"]
+    password = request.data["password"]
+
+    user = services.fetch_user_by_email(email=email)
+
+    if user is None:
+      raise exceptions.AuthenticationFailed("Invalid credentials.")
+
+    if not user.check_password(raw_password=password):
+      raise exceptions.AuthenticationFailed("Invalid credentials.")
+
+    token = services.create_token(user_id=user.id)
+
+    resp = response.Response()
+
+    resp.set_cookie(key="jwt", value=token, httponly=True)
+
+    return resp
+
+class UserAPIView(views.APIView):
+  authentication_classes = (authentication.CustomUserAuthentication,)
+  permission_classes = (permissions.IsAuthenticated,)
+
+  def get(self, request):
+    user = request.user
+
+    serializer = UserSerializer(user)
+
+    return response.Response(serializer.data)
+
+class LogoutAPIView(views.APIView):
+  authentication_classes = (authentication.CustomUserAuthentication,)
+  permission_classes = (permissions.IsAuthenticated,)
+
+  def post(self, request):
+    resp = response.Response()
+    resp.delete_cookie("jwt")
+    resp.data = {"message": "User logout."}
+
+    return resp
