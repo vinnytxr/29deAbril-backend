f40e23a2ec5462f6a46670f71ff780dfb59ce5e3
RenanGAS <renansakaoki@gmail.com>
2023-04-15 19:09:54 -0300

    #20: utilização do timezone local para cálculo da data de expiração do token

--- portal-aulas-api/user/services.py
+++ portal-aulas-api/user/services.py
@@ -2,6 +2,7 @@ import datetime
 import jwt
 from . import models
 from django.conf import settings
+from django.utils import timezone
 import pytz
 
 def fetch_user_by_email(email: str)->"User":
@@ -12,7 +13,7 @@ def fetch_user_by_email(email: str)->"User":
 def create_token(user_id: int)->str:
     payload = {
         "id":user_id,
-        "exp":datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(hours=24)
+        "exp":datetime.datetime.now(tz=timezone.get_current_timezone()) + datetime.timedelta(hours=24)
     }
 
     token=jwt.encode(payload, settings.JWT_SECRET, algorithm="HS256")
