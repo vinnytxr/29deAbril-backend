0e3d9b6b2e839960238abe48bb891413a15c5d9b
alessandro <git.aleneves@gmail.com>
2023-04-15 04:48:36 -0300

    #13: atualização metodo PUT e PATCH dos endpoints de Lessons
    Foi atualizado os metodos update e partial_update da viewset de Lessons para garantir que durante o update de um banner da Lesson a imagem antiga seja removida do disco para então substitui-la pela nova imagem

--- portal-aulas-api/lessons/views.py
+++ portal-aulas-api/lessons/views.py
@@ -7,3 +7,26 @@ from .serializers import LessonSerializer
 class LessonViewSet(viewsets.ModelViewSet):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
+    # [UPDATE] /<id> 
+    # body: multipart/form-data
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        # Verifica se há uma nova imagem na requisição
+        if 'banner' in request.FILES:
+            # Exclui a imagem antiga
+            instance.banner.delete(save=False)
+            # Salva a nova imagem
+            instance.banner = request.FILES['banner']
+
+        self.perform_update(serializer)
+
+        return Response(serializer.data)
+    
+    # [PATCH] /<id> 
+    # body: multipart/form-data
+    def partial_update(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
