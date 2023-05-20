970be6c649dbb88602032d9af726e4bbf9de49b2
Iago Carmona <iagoortegacarmona@gmail.com>
2023-05-20 16:55:54 -0300

    #45 - Adicionando url para favoritar um curso

--- portal-aulas-api/courses/urls.py
+++ portal-aulas-api/courses/urls.py
@@ -1,14 +1,14 @@
 from rest_framework import routers
 from django.urls import path, include
-from .views import CourseViewSet, LearningViewSet, FavoriteCourseAPIView
+from .views import CourseViewSet, LearningViewSet, FavoriteCourseViewSet
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'courses', CourseViewSet)
 router.register(r'learnings', LearningViewSet)
+router.register(r'favorites', FavoriteCourseViewSet, basename='favorite' )
 
 app_name = 'courses'
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('favorite', FavoriteCourseAPIView.as_view(), name="favorite-course"),
 ]
