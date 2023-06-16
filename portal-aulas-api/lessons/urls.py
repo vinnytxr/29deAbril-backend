a7f97815077b3e46403c9663ad23f2a2c71e4223
Iago Carmona <iagoortegacarmona@gmail.com>
2023-06-15 21:58:41 -0300

    #80 - Criando urls para comments

--- portal-aulas-api/lessons/urls.py
+++ portal-aulas-api/lessons/urls.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 from django.urls import path, include
-from .views import LessonViewSet
+from .views import LessonViewSet, CommentViewSet
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'lessons', LessonViewSet)
@@ -8,5 +8,8 @@ router.register(r'lessons', LessonViewSet)
 app_name = 'lessons'
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('lessons/<int:lesson_id>/comments/', CommentViewSet.as_view({'post': 'create'}), name='lesson-comments'),
+    path('lessons/<int:lesson_id>/comments/list', CommentViewSet.as_view({'get': 'list'}), name='list-comments'),
+    path('lessons/<int:lesson_id>/comments/<int:pk>/reply', CommentViewSet.as_view({'post': 'create_reply'}), name='reply-comment'),
 ]
