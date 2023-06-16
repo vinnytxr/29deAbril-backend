58b372897826d49f040f428ec033cfe0bf490c66
Iago Carmona <iagoortegacarmona@gmail.com>
2023-06-15 21:58:26 -0300

    #80 - Criando serializer para comments

--- portal-aulas-api/lessons/serializers.py
+++ portal-aulas-api/lessons/serializers.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
-from .models import Lesson
+from .models import Lesson, Comment
 # from courses.serializers.course import CourseResumeSerializer
 
 # from user.serializers import UserResumeSerializer
@@ -41,3 +41,16 @@ class LessonResumeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = ['id', 'title', 'banner']
+        
+class CommentReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'content', 'user', 'lesson']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    replies = CommentReplySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'content', 'user', 'lesson', 'replies']
