8880c878ac9435edf02d80fb5b079af47e8341af
Iago Carmona <iagoortegacarmona@gmail.com>
2023-06-23 19:24:28 -0300

    #84 - Atualizando model de lesson para receber apendice

--- portal-aulas-api/lessons/models.py
+++ portal-aulas-api/lessons/models.py
@@ -15,12 +15,20 @@ def get_video_path(instance, filename):
     filename = f"{uuid.uuid4().hex}.{ext}"
     return os.path.join('videos/courses/lessons', filename)
 
+def get_appendix_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f"{uuid.uuid4().hex}.{ext}"
+    return os.path.join('appendices/courses/lessons', filename)
+
+
 class Lesson(models.Model):
     id = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=64, blank=False)
     content = models.CharField(max_length=1024, blank=False)
     banner = models.FileField(upload_to=get_file_path, null=True, blank=True)
     video = models.FileField(upload_to=get_video_path, null=True, blank=True)
+    appendix = models.FileField(upload_to=get_appendix_path, null=True, blank=True)
+    title_appendix = models.CharField(max_length=64, null=True, blank=False)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons', null=False, blank=False)
     users_who_completed = models.ManyToManyField(User, blank=True)
 
