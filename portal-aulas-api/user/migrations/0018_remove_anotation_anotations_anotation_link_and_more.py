d1e9bdb81478d7fe3073fb2186bd61d274f9dc5b
RenanGAS <renansakaoki@gmail.com>
2023-06-26 12:34:00 -0300

    #77: muda campo do tempo do video para float e exclui o link

--- portal-aulas-api/user/migrations/0018_remove_anotation_anotations_anotation_link_and_more.py
+++ portal-aulas-api/user/migrations/0018_remove_anotation_anotations_anotation_link_and_more.py
@@ -27,7 +27,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='anotation',
             name='time',
-            field=models.TimeField(null=True, verbose_name='Tempo'),
+            field=models.FloatField(null=True, verbose_name='Tempo'),
         ),
         migrations.AddField(
             model_name='anotation',
