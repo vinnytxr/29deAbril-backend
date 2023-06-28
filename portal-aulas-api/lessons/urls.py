595bf7ca1c167c7078780309e2c9de5129e7fa09
Iago Carmona <iagoortegacarmona@gmail.com>
2023-06-28 20:37:42 -0300

    Atualizando urls

--- portal-aulas-api/lessons/urls.py
+++ portal-aulas-api/lessons/urls.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 from django.urls import path, include
-from .views import LessonViewSet, CommentViewSet
+from .views import LessonViewSet, CommentViewSet, ReplyViewSet
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'lessons', LessonViewSet)
@@ -12,5 +12,6 @@ urlpatterns = [
     path('lessons/<int:lesson_id>/comments/', CommentViewSet.as_view({'post': 'create'}), name='lesson-comments'),
     path('lessons/<int:lesson_id>/comments/list', CommentViewSet.as_view({'get': 'list'}), name='list-comments'),
     path('lessons/<int:lesson_id>/comments/<int:pk>/', CommentViewSet.as_view({'put': 'update', 'delete': 'destroy'}), name='comment-detail'),
-    path('lessons/<int:lesson_id>/comments/<int:pk>/reply', CommentViewSet.as_view({'post': 'create_reply'}), name='reply-comment'),
+    path('lessons/<int:lesson_id>/comments/<int:comment_id>/reply/<int:pk>/', ReplyViewSet.as_view({'put': 'update', 'delete': 'destroy'}), name='reply-detail'),
+    path('lessons/<int:lesson_id>/comments/<int:pk>/reply', ReplyViewSet.as_view({'post': 'create'}), name='reply-comment'),
 ]
